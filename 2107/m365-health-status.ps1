c419b9d690f9b80bba41f1cef8eae4b6579ae30a
Arjun U Menon <arjun.umenon@gmail.com>
2021-02-03 21:55:36 +0530

    Added field additions

--- 2107/m365-health-status.ps1
+++ 2107/m365-health-status.ps1
@@ -7,19 +7,19 @@ $CurrentList = (m365 spo list get --title $listName --webUrl $siteURL --output j
 if($CurrentList -eq $null){
     Write-Host "Creating the List"
 
-    #Creating the list
-    #m365 spo list add  --baseTemplate GenericList --title $listName --webUrl  $siteURL
+    #Creating the list - Conventional
+    m365 spo list add  --baseTemplate GenericList --title $listName --webUrl  $siteURL
 
-    m365 spo list add --title $listName --baseTemplate GenericList --webUrl $siteURL --schemaXml "" --debug
     #Adding the fields
-    #m365 spo field add --webUrl $siteURL --listTitle 'M365 Health Status' --xml "<Field Type='Text' DisplayName='Work Load' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' ID='{6085e32a-339b-4da7-ab6d-c1e013e5ab27}' SourceID='{4f118c69-66e0-497c-96ff-d7855ce0713d}' StaticName='workLoad' Name='workLoad' InternalName='workLoad'></Field>"
-
-    #m365 spo field add --webUrl $siteURL --listTitle 'M365 Health Status' --xml "<Field AppendOnly='FALSE' DisplayName='WorkflowJSONData' Format='Dropdown' IsModern='TRUE' IsolateStyles='FALSE' RichText='FALSE' RichTextMode='Compatible' Type='Note' ID='{9fb30bda-51dd-4c2f-96f5-25c96c04db44}' SourceID='{3209092b-1b9b-4742-9e6e-6a71a2b7ca0b}' StaticName='WorkflowJSONData' ColName='ntext2' RowOrdinal='0' />"
-
+    m365 spo field add --webUrl $siteURL --listTitle 'M365 Health Status' --xml "<Field Type='Text' DisplayName='WorkLoad' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='workLoad' Name='workLoad'></Field>" --options  AddFieldToDefaultView
+    m365 spo field add --webUrl $siteURL --listTitle 'M365 Health Status' --xml "<Field Type='DateTime' DisplayName='FirstIdentifiedDate' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='FirstIdentifiedDate' Name='FirstIdentifiedDate'></Field>" --options  AddFieldToDefaultView
+    m365 spo field add --webUrl $siteURL --listTitle 'M365 Health Status' --xml "<Field Type='Note' DisplayName='WorkflowJSONData' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='WorkflowJSONData' Name='WorkflowJSONData'></Field>" --options  AddFieldToDefaultView
+    m365 spo field add --webUrl $siteURL --listTitle 'M365 Health Status' --xml "<Field Type='Boolean' DisplayName='StillinOutage' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='StillinOutage' Name='StillinOutage'></Field>" --options  AddFieldToDefaultView
 }
 else{
     #DUMMY SCRIPT - REMOVE LIST
     m365 spo list remove --webUrl $siteURL --title $listName --confirm
     ##ND OF DUMMY
 }
+
 
