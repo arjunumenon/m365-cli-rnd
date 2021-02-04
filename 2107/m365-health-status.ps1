28bf376ee57e15c238e3ce0fae03db9ab28c6b45
Arjun U Menon <arjun.umenon@gmail.com>
2021-02-04 15:46:53 +0530

    Working Code - PowerShell

--- 2107/m365-health-status.ps1
+++ 2107/m365-health-status.ps1
@@ -1,25 +1,58 @@
-$siteURL = "https://aum365.sharepoint.com/sites/M365CLI"
+$webURL = "https://aum365.sharepoint.com/sites/M365CLI"
 $listName = "M365 Health Status"
 
-$CurrentList = (m365 spo list get --title $listName --webUrl $siteURL --output json) | ConvertFrom-Json
+$CurrentList = (m365 spo list get --title $listName --webUrl $webURL --output json) | ConvertFrom-Json
 
 #Checking List exists. Will create the list if the List doest not exist
 if($CurrentList -eq $null){
     Write-Host "Creating the List"
 
     #Creating the list - Conventional
-    m365 spo list add  --baseTemplate GenericList --title $listName --webUrl  $siteURL
+    $CurrentList = m365 spo list add  --baseTemplate GenericList --title $listName --webUrl  $webURL
 
     #Adding the fields
-    m365 spo field add --webUrl $siteURL --listTitle 'M365 Health Status' --xml "<Field Type='Text' DisplayName='WorkLoad' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='workLoad' Name='workLoad'></Field>" --options  AddFieldToDefaultView
-    m365 spo field add --webUrl $siteURL --listTitle 'M365 Health Status' --xml "<Field Type='DateTime' DisplayName='FirstIdentifiedDate' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='FirstIdentifiedDate' Name='FirstIdentifiedDate'></Field>" --options  AddFieldToDefaultView
-    m365 spo field add --webUrl $siteURL --listTitle 'M365 Health Status' --xml "<Field Type='Note' DisplayName='WorkflowJSONData' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='WorkflowJSONData' Name='WorkflowJSONData'></Field>" --options  AddFieldToDefaultView
-    m365 spo field add --webUrl $siteURL --listTitle 'M365 Health Status' --xml "<Field Type='Boolean' DisplayName='StillinOutage' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='StillinOutage' Name='StillinOutage'></Field>" --options  AddFieldToDefaultView
+    $FieldLists = @(
+    @{fieldname="Workload";fieldtype="Text";},@{fieldname="FirstIdentifiedDate";fieldtype="DateTime";},@{fieldname="WorkflowJSONData";fieldtype="Note";}
+    )
+    Foreach ($field in $FieldLists){
+        $addedField = m365 spo field add --webUrl $webURL --listTitle 'M365 Health Status' --xml "<Field Type='$($field.fieldtype)' DisplayName='$($field.fieldname)' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='$($field.fieldname)' Name='$($field.fieldname)'></Field>" --options  AddFieldToDefaultView
+    }
 }
 else{
     #DUMMY SCRIPT - REMOVE LIST
-    m365 spo list remove --webUrl $siteURL --title $listName --confirm
+    #m365 spo list remove --webUrl $webURL --title $listName --confirm
     ##ND OF DUMMY
 }
+
+#return
+#Health Status Setup
+$workLoads = m365 tenant status list --query "value[?Status != 'ServiceOperational']"  --output json  | ConvertFrom-Json
+$currentOutageServices = (m365 spo listitem list --webUrl $webURL --title $listName --fields "Title, Workload, Id"  --output json).Replace("ID", "_ID") | ConvertFrom-Json
+
+$notifyEmail = "arjun@aum365.onmicrosoft.com"
+
+#Checking for any new outages
+Foreach ($workload in $workLoads){
+    if($workload.Workload -notin $currentOutageServices.Workload){
+        #Add outage information to SharePoint List
+        $addedWorkLoad = m365 spo listitem add --webUrl $webURL --listTitle $listName --contentType Item --Title $workload.WorkloadDisplayName --Workload $workload.Workload --FirstIdentifiedDate (Get-Date -Date $workload.StatusTime -Format "MM/dd/yyyy HH:mm") --WorkflowJSONData (Out-String -InputObject $workload -Width 100)
+
+        #Send notification using CLI Commands
+        m365 outlook mail send --to $notifyEmail --subject "Outage Reported in $($workload.WorkloadDisplayName)" --bodyContents "Any outage has been reported for the Service : $($workload.WorkloadDisplayName) <a href='$webURL/Lists/$listName'>Access the Health Status List</a>" --bodyContentType HTML --saveToSentItems false
+    }
+}
+
+#Checking whether any existing outages are resolved
+Foreach ($Service in $currentOutageServices){
+    if($Service.Workload -notin $workLoads.Workload){
+
+        #Removing the outage information from SharePoint List
+        $removedRecord = m365 spo listitem remove --webUrl $webURL --listTitle $listName --id  $Service.Id --confirm
+
+        #Send notification using CLI Commands
+        m365 outlook mail send --to $notifyEmail --subject "Outage RESOLVED for $($Service.Title)" --bodyContents "Outage which was reported for the Service : $($Service.Title) is RESOLVED." --bodyContentType HTML --saveToSentItems false
+    }
+}
+
 
 
