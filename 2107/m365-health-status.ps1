180eb2e368724a1f1119fc5b36a203538fef21ce
Arjun U Menon <arjun.umenon@gmail.com>
2021-02-04 15:51:19 +0530

    Cleaned the code

--- 2107/m365-health-status.ps1
+++ 2107/m365-health-status.ps1
@@ -3,13 +3,12 @@ $listName = "M365 Health Status"
 
 $CurrentList = (m365 spo list get --title $listName --webUrl $webURL --output json) | ConvertFrom-Json
 
-#Checking List exists. Will create the list if the List doest not exist
+#Checking whether List exists. Will create the list if the List doest not exist
 if($CurrentList -eq $null){
-    Write-Host "Creating the List"
+    Write-Host "List does not exist. Hence creating the SharePoint List"
 
     #Creating the list - Conventional
     $CurrentList = m365 spo list add  --baseTemplate GenericList --title $listName --webUrl  $webURL
-
     #Adding the fields
     $FieldLists = @(
     @{fieldname="Workload";fieldtype="Text";},@{fieldname="FirstIdentifiedDate";fieldtype="DateTime";},@{fieldname="WorkflowJSONData";fieldtype="Note";}
@@ -18,14 +17,8 @@ if($CurrentList -eq $null){
         $addedField = m365 spo field add --webUrl $webURL --listTitle 'M365 Health Status' --xml "<Field Type='$($field.fieldtype)' DisplayName='$($field.fieldname)' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='$($field.fieldname)' Name='$($field.fieldname)'></Field>" --options  AddFieldToDefaultView
     }
 }
-else{
-    #DUMMY SCRIPT - REMOVE LIST
-    #m365 spo list remove --webUrl $webURL --title $listName --confirm
-    ##ND OF DUMMY
-}
 
-#return
-#Health Status Setup
+#Getting current Tenant Status and do the needed operations
 $workLoads = m365 tenant status list --query "value[?Status != 'ServiceOperational']"  --output json  | ConvertFrom-Json
 $currentOutageServices = (m365 spo listitem list --webUrl $webURL --title $listName --fields "Title, Workload, Id"  --output json).Replace("ID", "_ID") | ConvertFrom-Json
 
@@ -53,6 +46,3 @@ Foreach ($Service in $currentOutageServices){
         m365 outlook mail send --to $notifyEmail --subject "Outage RESOLVED for $($Service.Title)" --bodyContents "Outage which was reported for the Service : $($Service.Title) is RESOLVED." --bodyContentType HTML --saveToSentItems false
     }
 }
-
-
-
