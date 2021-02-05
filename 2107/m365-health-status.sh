f7bec095d56b01ca908b8901004ba6a09df359cb
Arjun U Menon <arjun.umenon@gmail.com>
2021-02-05 17:33:17 +0530

    Partially done for that

--- 2107/m365-health-status.sh
+++ 2107/m365-health-status.sh
@@ -1,6 +1,29 @@
-defaultIFS=$IFS
-IFS=$'\n'
+# defaultIFS=$IFS
+# IFS=$'\n'
 
-availableTeams=$(m365 tenant status list --query "value[?Status != 'ServiceOperational']"  --output json)
 
-echo $availableTeams
+FieldLists='[{"fieldname":"Workload","fieldtype":"Text"},{"fieldname":"FirstIdentifiedDate","fieldtype":"DateTime"}]'
+
+
+echo "${FieldLists}" | jq '.[].fieldtype'
+
+# # jq -c '.[]' $FieldLists.json | while read i; do
+# #     echo $i
+# # done
+
+# echo "${FieldLists}" | jq | while read i; do
+#     echo "arjun"
+# done
+
+return
+
+
+webURL="https://aum365.sharepoint.com/sites/M365CLI"
+listName="M365 Health Statu"
+CurrentList=$(m365 spo list get --webUrl $webURL --title "$listName" --output json)
+
+if [ -z "$CurrentList" ]
+then
+      echo "List does not exist. Hence creating the SharePoint List"
+      CurrentList=$(m365 spo list add --baseTemplate GenericList --webUrl $webURL --title "$listName")
+fi
