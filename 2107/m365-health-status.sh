9bb71e6418b2e45de60e5f8f532367ce4d8847c7
Arjun U Menon <arjun.umenon@gmail.com>
2021-02-08 19:12:34 +0530

    Issue in Reading through Items

--- 2107/m365-health-status.sh
+++ 2107/m365-health-status.sh
@@ -18,3 +18,13 @@ then
             addedField=$(m365 spo field add --webUrl $webURL --listTitle "$listName" --xml "<Field Type='$(echo $field | jq -r ''.fieldtype)' DisplayName='$(echo $field | jq -r ''.fieldname)' Required='FALSE' EnforceUniqueValues='FALSE' Indexed='FALSE' StaticName='$(echo $field | jq -r ''.fieldname)' Name='$(echo $field | jq -r ''.fieldname)'></Field>" --options  AddFieldToDefaultView)
       done
 fi
+
+#Getting current status and do the needed operation
+workLoads=$(m365 tenant status list --query "value[?Status != 'ServiceOperational']"  --output json)
+currentOutageServices=$(m365 spo listitem list --webUrl $webURL --title "$listName" --fields "Title, Workload, Id"  --output json)
+
+#Checking for any new outages
+for workLoad in $(echo $workLoads | jq -c '.[]'); do
+      echo $workLoad
+      echo $(echo $workLoad | jq -r ''.Id)
+done
