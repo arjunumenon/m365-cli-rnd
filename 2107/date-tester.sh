c4bd9e95b6289ee5d095398b9cd84ff816bc99cf
Arjun Menon <arjun.umenon@gmail.com>
2021-03-02 18:41:47 +0530

    Removed unwanted code. Final push

--- 2107/date-tester.sh
+++ 2107/date-tester.sh
@@ -1,11 +1,3 @@
-# dateTemp="2021-02-25T14:16:56.1242048Z"
-
-# #startd=$(date -d "$dateTemp" "+%Y-%m-%d %H:%M:%S")
-
-# #SharePoint Date
-# startd=$(date -d "$dateTemp" '+%m/%d/%Y %H:%M:%S')
-
-
 #Working approch in both Unix and Macos
 restDateValue="2021-02-25T14:16:56.1242048Z"
 
