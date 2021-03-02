20890cc24804786efe680fdda6cd5c1c9cdd5ad2
Arjun Menon <arjun.umenon@gmail.com>
2021-03-02 12:50:55 +0530

    Final working code. Will wait for other review

--- 2107/date-tester.sh
+++ 2107/date-tester.sh
@@ -1,10 +1,22 @@
-dateTemp="2021-02-25T14:16:56.1242048Z"
+# dateTemp="2021-02-25T14:16:56.1242048Z"
 
-#startd=$(date -d "$dateTemp" "+%Y-%m-%d %H:%M:%S")
+# #startd=$(date -d "$dateTemp" "+%Y-%m-%d %H:%M:%S")
 
-#SharePoint Date
-startd=$(date -d "$dateTemp" '+%m/%d/%Y %H:%M:%S')
+# #SharePoint Date
+# startd=$(date -d "$dateTemp" '+%m/%d/%Y %H:%M:%S')
 
 
+restDateValue="2021-02-25T14:16:56.1242048Z"
 
-echo $startd
+epochTimeDate=$(date -d "$restDateValue" +%s)
+
+formattedDate=$(date -d @$(echo $epochTimeDate) '+%m/%d/%Y %H:%M:%S')
+
+echo $formattedDate
+
+# startd=$(date -j -f "%Y-%m-%d %H:%M" "2015-06-11 12:39")
+
+
+# startd=$(date -d "$dateTemp" +%F2021)
+
+# echo $startd)  %H
