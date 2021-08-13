120223ea91666d896201f0a5f65bb8a85b522f0e
Arjun Menon <arjun.umenon@tcs.com>
2021-08-13 20:14:45 +0530

    Keeping on hold till command is completed.

--- 2219/m365-group-membership.ps1
+++ 2219/m365-group-membership.ps1
@@ -2,6 +2,17 @@
 
 # Write-Host $SiteList
 
+#m365 spo site list --query "[]"
 #m365 spo site list --output json --query "[]"
 #m365 spo site list --output json --query "[?Title == 'DEV-IceBreaker']"
-m365 spo site list --output json --query "[? contains(Title,'DEV')]"
+#m365 spo site list --output json --query "[? contains(Title,'m365')]"
+
+$siteFilterKeyword = "Corporate"
+#m365 spo site list --output json --query "[? contains(Title,'$siteFilterKeyword')]"
+
+
+$siteList = m365 spo site list --output json --query "[? contains(Title,'$siteFilterKeyword')]" | ConvertFrom-Json
+
+Foreach ($site in $siteList){
+    Write-Host "Prcessing the site - $($site.Url)"
+}
