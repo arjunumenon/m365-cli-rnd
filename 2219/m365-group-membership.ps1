0ad7d1a95f6144ca17dfeee23be56d0c2ccbecd7
Arjun Menon <arjun.umenon@gmail.com>
2022-03-15 18:02:14 +0530

    Final working code. Refractoring working

--- 2219/m365-group-membership.ps1
+++ 2219/m365-group-membership.ps1
@@ -1,16 +1,5 @@
-# $SiteList = m365 spo site list
-
-# Write-Host $SiteList
-
-#m365 spo site list --query "[]"
-#m365 spo site list --output json --query "[]"
-#m365 spo site list --output json --query "[?Title == 'DEV-IceBreaker']"
-#m365 spo site list --output json --query "[? contains(Title,'m365')]"
-
 $siteFilterKeyword = "SPFX"
-#m365 spo site list --output json --query "[? contains(Title,'$siteFilterKeyword')]"
 
-# $siteList = m365 spo site list --output json --query "[? contains(Title,'$siteFilterKeyword')]" | ConvertFrom-Json
 $siteList = m365 spo site list  --type CommunicationSite  --filter "Url -like '$siteFilterKeyword'" --output json | ConvertFrom-Json
 $TotalSiteCount = $SiteList.Count
 $SiteCounter = 1
@@ -21,28 +10,17 @@ Foreach ($site in $siteList){
     # Getting only Associated Owner and Member Groups using JMES Query
     $AssociatedGroups = m365 spo web get --webUrl $site.Url --withGroups --query "{MemberGroup: AssociatedMemberGroup, OwnerGroup: AssociatedOwnerGroup}" --output json | ConvertFrom-Json
 
-    # Getting list of members from the Group
-    # Conventional Approach
+    # Getting list of members from the Owner Group
     $UserList = m365 spo group user list --webUrl $site.Url --groupId $AssociatedGroups.OwnerGroup.Id --query "value" --output json | ConvertFrom-Json
 
-    Write-Host "Total Users available in the Group : "$UserList.Count
-    $UserlistToBeAdded =  [System.Collections.ArrayList]@()
+    Write-Host "Total Users available in the Group, $($AssociatedGroups.OwnerGroup.Title) : "$UserList.Count
     Foreach ($User in $UserList){
-        # Add Users to Member Group
-        write-Host "Adding User $($User.UserPrincipalName) to the Group $($AssociatedGroups.MemberGroup.Id)"
-        
+
         # Single User Adding
         m365 spo group user add --webUrl $site.Url --groupId $AssociatedGroups.MemberGroup.Id --userName "$($User.UserPrincipalName)"
-        
-        # # Adding to Arraylist and adding command will be executed later since it supports multiple - 
-        # # user addition in single command call
-        # $UserlistToBeAdded+=$User.UserPrincipalName
         
         # Removing the user from SharePoint Group
         m365 spo group user remove --webUrl $site.Url --groupId $AssociatedGroups.OwnerGroup.Id --userName "$($User.UserPrincipalName)" --confirm
     }
-
-    # Write-Host ($UserlistToBeAdded -join ", ")
-    # m365 spo group user add --webUrl $site.Url --groupId $AssociatedGroups.MemberGroup.Id --userName "$UserlistToBeAdded" --debug
     $SiteCounter++
 }
