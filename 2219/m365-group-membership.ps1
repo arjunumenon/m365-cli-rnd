e40e5e8c875852c66aed181aab219fa364cee663
Arjun Menon <arjun.umenon@gmail.com>
2022-03-16 13:18:19 +0530

    Working script - Final

--- 2219/m365-group-membership.ps1
+++ 2219/m365-group-membership.ps1
@@ -1,12 +1,13 @@
-$URLFilterKeyWord = "SPFX"
+$siteFilterKeyword = "Central"
 
-#Getting the Communication Sites which has the URL Filter "SPFX"
-$siteList = m365 spo site list  --type CommunicationSite  --filter "Url -like '$URLFilterKeyWord'" --output json | ConvertFrom-Json
+#Getting the Communication Sites whose title contains the keyword "SPFX"
+$siteList = m365 spo site list --type CommunicationSite --output json --query "[? contains(Title,'$siteFilterKeyword')]" | ConvertFrom-Json
 $TotalSiteCount = $SiteList.Count
+Write-Host "Total number sites which has the keyword '$siteFilterKeyword' in their title are : $TotalSiteCount"
 $SiteCounter = 1
 
 Foreach ($site in $siteList){
-    Write-Host "Processing the site No : $SiteCounter / $TotalSiteCount."
+    Write-Host "Processing site No : $SiteCounter / $TotalSiteCount."
     Write-Host "Site URL - $($site.Url)"
     # Getting only Associated Owner and Member Groups using JMES Query
     $AssociatedGroups = m365 spo web get --webUrl $site.Url --withGroups --query "{MemberGroup: AssociatedMemberGroup, OwnerGroup: AssociatedOwnerGroup}" --output json | ConvertFrom-Json
@@ -17,10 +18,10 @@ Foreach ($site in $siteList){
     Write-Host "Total Users available in the Group, $($AssociatedGroups.OwnerGroup.Title) : "$UserList.Count
     Foreach ($User in $UserList){
 
-        # Single User Adding
+        # Adding the user to Member Group
         m365 spo group user add --webUrl $site.Url --groupId $AssociatedGroups.MemberGroup.Id --userName "$($User.UserPrincipalName)"
         
-        # Removing the user from SharePoint Group
+        # Removing the user from Owner Group
         m365 spo group user remove --webUrl $site.Url --groupId $AssociatedGroups.OwnerGroup.Id --userName "$($User.UserPrincipalName)" --confirm
     }
     $SiteCounter++
